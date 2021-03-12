5737672bdae242282dbcbec57b78b0da46c64793
Hodaya Siman Tov <hodaysi123@gmail.com>
2021-03-12 14:31:04 +0200

    init

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,5 @@
 function newHome(){
 
-    var myWindow = window.open("", "_self");
-    myWindow.document.write("<p>I replaced the current window.</p>");
+    var myWindow = window.open('', '_self');
+    myWindow.document.write('<p>I replaced the current window.</p>');
 }
