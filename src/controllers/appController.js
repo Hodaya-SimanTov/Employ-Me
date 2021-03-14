3bdb0cf19c873dc71b6ab18a1a1927ac1a3d6cd2
Hodaya Siman Tov <hodaysi123@gmail.com>
2021-03-14 14:23:33 +0200

    path

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -1,9 +1,10 @@
 module.exports = function(app){
     app.get('/',(req,res)=>{
-        res.render('login')
-    });
-    app.get('/homepage',(req,res)=>{
         res.render('homepage')
+        
+    });
+    app.get('/login',(req,res)=>{
+        res.render('login')
     });
     app.get('/sign-up',(req,res)=>{
         res.render('sign_up')
