418828317d7c847f7f85197cd925c371df7a7ae8
Hodaya Siman Tov <hodaysi123@gmail.com>
2021-04-11 21:04:29 +0300

    firebase

---
+++ views/sign_up.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAYFylGCNTuv_WPEn6YN9QaK3B7utgLwns",
+    authDomain: "employ-me-6f74e.firebaseapp.com",
+    projectId: "employ-me-6f74e",
+    storageBucket: "employ-me-6f74e.appspot.com",
+    messagingSenderId: "403952758032",
+    appId: "1:403952758032:web:3fb3b238c7ffd59d78c995",
+    measurementId: "G-V57RLMYD39"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
