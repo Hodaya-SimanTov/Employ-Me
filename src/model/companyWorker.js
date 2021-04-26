a70cf86c527d145a80653f4d8348e2d97f19978a
TalFarhan <62951476+TalFarhan@users.noreply.github.com>
2021-04-26 13:04:25 +0300

    sprint3_c_w

---
+++ src/model/companyWorker.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose')
+
+const CompanyWorkerSchema=mongoose.Schema({
+    firstName:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        require:true
+    },
+    mail:{
+        type:String,
+        require:true
+    },
+    phone:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true,
+        minlength:6
+    },
+    rule:{
+        type:String,
+        default:0
+    }
+})
+module.exports=mongoose.model('CompanyWorker',CompanyWorkerSchema)
